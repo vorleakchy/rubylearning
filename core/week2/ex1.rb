f389f2258c487445b0cb70ec6c893cc9d717a343
Vorleak Chy <vorleak.chy@gmail.com>
2011-05-28 23:36:22 +0700

    add exercise 1

---
+++ core/week2/ex1.rb
@@ -0,0 +1,21 @@
+=begin
+
+  Exercise1. Write a program that processes the string s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n" a line at a time, using all that we have learned so far. The expected output is:
+
+  >ruby tmp.rb
+  Line 1: Welcome to the forum.
+  Line 2: Here you can learn Ruby.
+  Line 3: Along with other members.
+  >Exit code: 0
+	
+=end
+
+s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
+
+s.lines.inject(1) do |line_number, text| 
+  puts "Line #{line_number}: #{text}" 
+  line_number.next
+end
+
+# Or
+s.lines.inject(1) { |line_number, text| puts "Line #{line_number}: #{text}"; line_number.next}
